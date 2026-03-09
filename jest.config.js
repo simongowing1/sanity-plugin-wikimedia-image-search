2d78f7031c758ee08166db17946c9639cc287160
simongowing1 <simongowing1@gmail.com>
2026-03-09 14:58:15 +0100

    feat: add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+};
